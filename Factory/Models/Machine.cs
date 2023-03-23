3ea20f6f8239562f401f542d13b02bb00e71d646
Geoffrey Gao <geoffreygao1@gmail.com>
2023-03-23 15:46:44 -0700

    Updated validation for machine title

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "The machine's name can't be empty!")]
     public string Title { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
