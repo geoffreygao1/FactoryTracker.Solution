db903ce9c1d94a150c4f3329f61286f5562ad2b3
Geoffrey Gao <geoffreygao1@gmail.com>
2023-03-23 14:30:19 -0700

    Initialized create view for engineer controller

---
+++ Factory/Migrations/20230323212955_UpdateEngineerName.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class UpdateEngineerName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Engineers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Engineers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Engineers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
-    public string Title { get; set; }
+    [Required(ErrorMessage = "The engineer's name can't be empty!")]
+    public string Name { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
 }
