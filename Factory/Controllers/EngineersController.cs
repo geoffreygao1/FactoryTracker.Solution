46239e28ee79798ed44d54717f8002f3ddbf7a96
Geoffrey Gao <geoffreygao1@gmail.com>
2023-03-23 14:31:48 -0700

    Added details get route

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -43,5 +43,14 @@ namespace Factory.Controllers
       }
     }
 
+    public ActionResult Details(int id)
+    {
+      Engineer thisEngineer = _db.Engineers
+          .Include(engineer => engineer.JoinEntities)
+          .ThenInclude(join => join.Machine)
+          .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
   }
 }
